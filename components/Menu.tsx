7bbc0523ff054239a45dc270aa0475e6158ac9e2
toke8677 <toke8677@gmail.com>
2021-08-23 14:27:49 +0300

    Got profile picture menu working and added link to black button

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,6 +1,16 @@
 import imgSources from '../services/imgSources';
-import { useState } from "react";
+import {useRef, RefObject, useState, useEffect} from "react";
 import { AccountDropdown } from './modules/AccountDropdown'
+
+function detectClickOutside(ref: RefObject<any>, action: Function) {
+    useEffect(() => {
+        const handleClick = (e: Event) => ref.current && !ref.current.contains(e.target) && action()
+
+        document.addEventListener("click", handleClick);
+        return () => document.removeEventListener("click", handleClick);
+
+    }, [ref]);
+}
 
 export default function Menu() {
     const [menuShown, setMenuShown] = useState(false);
@@ -8,15 +18,18 @@ export default function Menu() {
         setMenuShown(!menuShown)
     }
 
+    const menuRef = useRef(null);
+    detectClickOutside(menuRef, toggleMenu);
+
     return (
-        <div className='menu'>
+        <div className='menu' ref={menuRef}>
             <div className='logo'>BudgetGenix</div>
 
             <div onMouseDown={toggleMenu} className='account'>
                 <img src={imgSources.profile} alt='' />
             </div>
+            { menuShown && <AccountDropdown loggedIn={false} accountName={""} /> }
 
-            { menuShown && <AccountDropdown  accountName={""} accountUrl={""}/> }
 
         </div>
     );
--- components/modules/AccountDropdown.tsx
+++ components/modules/AccountDropdown.tsx
@@ -1,26 +1,31 @@
+import Link from 'next/link'
+
 const items = [
-    { name: "Your profile" },
-    { name: "Your budgets" },
-    { name: "Settings" },
-    { name: "Your profile" },
-    { name: "Your profile" },
-    { name: "Your profile" },
-    { name: "Your profile" },
-    { name: "Your profile" },
+    { id: 1, name: "Your profile", link: "" },
+    { id: 2, name: "Your budgets", link: "" },
+    { id: 3, name: "Settings", link: "" },
+    { id: 4, name: "Your profile", link: "" },
+    { id: 5, name: "Your profile", link: "" },
+    { id: 6, name: "Your profile", link: "" },
+    { id: 7, name: "Your profile", link: "" },
+    { id: 8, name: "Your profile", link: "" },
 ]
 
-
-interface accountInfo {
-    accountName: string
-    accountUrl: string
-}
-
-export function AccountDropdown(props: accountInfo) {
-    const accountName = props.accountName || "admin";
+export function AccountDropdown(props: { loggedIn: boolean, accountName: string }) {
+    const signedIn = props.loggedIn || false;
+    const accountName = props.accountName || "unknown";
     return (
         <div className='account-dropdown'>
             <div className="items">
-                <div className="welcome">Signed in as <span className="accountName">{accountName}</span></div>
+                <div className="welcome">
+                    { signedIn ? "You are signed in as" : "You are not signed in." }
+                    &nbsp;
+                    <Link href={signedIn ? `/user/${encodeURIComponent(accountName)}` : `login` }>
+                        <span className="accountName">
+                            {signedIn ? accountName : "Login"}
+                        </span>
+                    </Link>
+                </div>
                 <hr />
                 { items.map(item => <div key={Math.random()} className="item">{item.name}</div>) }
             </div>
