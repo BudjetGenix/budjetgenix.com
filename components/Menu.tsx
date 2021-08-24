b9e10c6260f50e018f257e98b7119909e2f3343e
toke8677 <toke8677@gmail.com>
2021-08-24 22:13:58 +0300

    working?! probably nice

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,34 +2,44 @@ import imgSources from '../services/imgSources';
 import {useRef, RefObject, useState, useEffect} from "react";
 import { AccountDropdown } from './modules/AccountDropdown'
 
-function detectClickOutside(ref: RefObject<any>, action: Function) {
+function listenOutsideClicks(menuRef: RefObject<any>, menuShown: boolean, toggleMenu: Function) {
     useEffect(() => {
-        const handleClick = (e: Event) => ref.current && !ref.current.contains(e.target) && action()
-
-        document.addEventListener("click", handleClick);
-        return () => document.removeEventListener("click", handleClick);
-
-    }, [ref]);
+        const fuc = (e: Event) => {
+            if (menuRef.current.contains(e.target)) return;
+            if (!menuShown) return;
+            toggleMenu()
+        }
+        document.addEventListener("mousedown", fuc);
+        return () => document.removeEventListener("mousedown", fuc)
+    }, [menuShown])
 }
 
 export default function Menu() {
     const [menuShown, setMenuShown] = useState(false);
-    function toggleMenu() {
-        setMenuShown(!menuShown)
+    const  toggleMenu = () => {
+        setMenuShown(!menuShown);
     }
 
     const menuRef = useRef(null);
-    detectClickOutside(menuRef, toggleMenu);
+    listenOutsideClicks(menuRef, menuShown, toggleMenu)
 
     return (
         <div className='menu' ref={menuRef}>
-            <div className='logo'>BudgetGenix</div>
+            <div className='logo'>BudgetGenie</div>
 
             <div onMouseDown={toggleMenu} className='account'>
                 <img src={imgSources.profile} alt='' />
             </div>
-            { menuShown && <AccountDropdown loggedIn={false} accountName={""} /> }
-
+            <div>
+                {
+                    menuShown
+                        ? <AccountDropdown
+                                loggedIn={false}
+                                accountName={""}
+                            />
+                        : <></>
+                }
+            </div>
 
         </div>
     );
